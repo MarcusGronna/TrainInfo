8c528da65d7b0f2cee1ff5fd6b87cc24034302ed
Marcus <mgronna@gmail.com>
2025-09-24 12:44:21 +0200

    clean repo of unwanted stuff

---
+++ WebApi/Controllers/TrainsController.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainsController : ControllerBase
+    {
+            private Mock_Db _db;
+        
+            
+            public TrainsController(Mock_Db db)
+            {
+            _db = db;
+            }
+
+        [HttpGet]
+        public List<Train> GetAll()
+        {
+            return _db.trainList;
+        }
+            
+        
+    }
+}
